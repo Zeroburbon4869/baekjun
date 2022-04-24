2618b563b24c587b7e1ee57667c976bbc1f5ba9b
차영진이다 <94175104+Zeroburbon4869@users.noreply.github.com>
2022-04-24 13:52:23 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,5 @@
-import java.util.Scanner;
-
 public class Main{
     public static void main(String[]args){
-        Scanner scanner = new Scanner(System.in);
-
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
-        scanner.close();
-        int C = A+B;
-
-        System.out.println(C);
+    System.out.println("Hello World!");
     }
 }
